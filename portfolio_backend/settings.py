e9693a3289d47f32543ef7ecb007837391768e4c
AbhishekYadav-01 <b22es020@iitj.ac.in>
2025-10-17 15:52:26 +0530

    feat: Configure CORS for production frontend

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -46,7 +46,13 @@ MIDDLEWARE = [
 
 # --- CORS Configuration ---
 # Allow all origins for simplicity, but you can restrict this to your frontend URL
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://portfolio-frontend-chi-sooty.vercel.app", 
+]
 
 ROOT_URLCONF = 'portfolio_backend.urls'
 
